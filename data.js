dbf374cd51f9e1b9f508fa0c932abc6dd4bbbdae
Subhodip Ray <raysubhodip1993@gmail.com>
2020-07-26 03:47:47 -0400

    Initial Commit

---
+++ data.js
@@ -0,0 +1,18 @@
+var data = [
+  { "skill":"Api-Designer", "url":"ABC ", "word":"Hello" },
+  { "skill":"Api-Designer", "url":"BCD", "word":"Hello1" },
+  { "skill":"Api-Designer", "url":"kkk", "word":"Hello2" },
+  { "skill":"Api-Designer", "url":"", "word":"Hello3" },
+  { "skill":"Api-Designer", "url":"", "word":"Hello4" },
+  { "skill":"Api-Designer", "url":"", "word":"Hello5" },
+{ "skill":"Api-Designer", "url":"", "word":"Hello6" },
+ { "skill":"Api-Designer", "url":"", "word":"Hello7" },
+{ "skill":"Api-Designer", "url":"", "word":"Hello8" },
+{ "skill":"Api-Designer", "url":"", "word":"Hello9" },
+{ "skill":"Api-Designer", "url":"", "word":"Hello10" },
+  { "skill":"Api-Designer", "url":"", "word":"Hello11" },
+  { "skill":"Api-Designer", "url":"", "word":"Hello12" },
+  { "skill":"Api-Designer", "url":"", "word":"Hello13" },  
+  { "skill":"Software Interface design", "url":"infoPages/interfaceDesign1.html#Prerequisites", "word":"HTML" , "section": "Prerequisites"}, 
+     
+];

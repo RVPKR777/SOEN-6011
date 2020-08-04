144c9d0326758ed7c2d10deced4b93224ae9ac6c
Rajan <erajan96@yahoo.com>
2020-08-04 08:42:08 -0600

    search functionality changes

--- dist/data.js
+++
@@ -1,4 +0,0 @@
-var data = [
-  { "skill":"Api-Programming", "url":"infoPages/apiProgramming1.html#Prerequisites", "word":"API", "section": "Prerequisites"},
-  { "skill":"Software Interface design", "url":"infoPages/interfaceDesign1.html#Prerequisites", "word":"HTML" , "section": "Prerequisites"}, 
-];

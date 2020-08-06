ede23d495b27f457714ca2e0608ffe6587e379fc
Rajan <erajan96@yahoo.com>
2020-08-06 11:42:36 -0600

    UI components added | search functionality added

---
+++ dist/components/navbar.js
@@ -0,0 +1,48 @@
+class NavBar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark lighten-5 mb-4">
+        <a class="navbar-brand" href="#">
+          <img src="${this.getAttribute("logoPath")}" width="30" height="30" alt="">
+        </a>
+        <a class="navbar-brand" href="#">Sestopia</a>
+    
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="../index.html">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">References</a>
+            </li>
+    
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+                aria-expanded="false">Skills</a>
+              <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">API Programming</a>
+                <a class="dropdown-item" href="#">Interface Design</a>
+                <a class="dropdown-item" href="#">Writing Unit Tests</a>
+                <a class="dropdown-item" href="#">Software Requirements</a>
+                <a class="dropdown-item" href="#">Software Quality</a>
+                <a class="dropdown-item" href="#">Software Configuration Management</a>
+                <a class="dropdown-item" href="#">Software Process & Life-Cycle</a>
+                <a class="dropdown-item" href="#">Software Maintanance Planning</a>
+              </div>
+            </li>
+          </ul>
+          <form class="search form-inline">
+            <input type="text" name="inputText" id="inputText" onkeyup="search()" class="form-control"
+              placeholder="Search..." />
+            <ul id="myUL" class="results"> </ul>
+          </form>
+        </div>
+      </nav>`
+    }
+}
+
+customElements.define("nav-bar",NavBar);
--- dist/info.js
+++ dist/info.js
@@ -1,61 +1,7 @@
-$(document).ready(function(){
-    $.each(data, function(i, item) {
-        console.log(data[i].url);
-        $("#myUL").append('<li><div onclick="location.href='+'stackoverflow.com/questions/2188272/html-how-to-make-an-entire-div-a-hyperlink/2188293'+';" id="smallbox">The content of the div here</div></li>');
-        console.log("Appended");
-    });
-  });
-
-
-function inpOnFocus(){
-    document.getElementById("q").placeholder =">> Word "+
-    " >> Section "+
-    " >> Skill ";
-    
-
-}
-
-
-function inpOutFocus(){
-    document.getElementById("q").placeholder ="Search ...";
-}
-
-
-function searchFx() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    var count = 0;
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-            count++;
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-    console.log(count);
-    if(count === 0) {
-        li[0].style.display ="No Searches Found";
-    }
-}
-
-function getValue(){
-    
-    var sel = document.getElementById('myUL');
-    //var val= sel.value;
-    var opt = sel.options[sel.selectedIndex];
-    console.log(opt.value);
-    window.location.replace(opt.value);
-}
-
-
 const searchFor = {
     "api": [{ href: "infoPages/apiProgramming1.html#Prerequisites", innerText: "What is an API?" },
+    { href: "infoPages/apiProgramming2.html#Roles", innerText: "Robust API" },
+    { href: "infoPages/apiProgramming2.html#Roles", innerText: "Robust API" },
     { href: "infoPages/apiProgramming2.html#Roles", innerText: "Robust API" }],
     "apiProgramming": [{ href: "infoPages/apiProgramming1.html#Prerequisites", innerText: "What is an API?" },
     { href: "infoPages/apiProgramming2.html#Roles", innerText: "Robust API" }],
@@ -80,21 +26,24 @@ const getKeysArray = (searchKey) => {
 }
 
 const search = () => {
-    const ul = document.getElementById("search");
+    const ul = document.getElementById("myUL");
     ul.innerHTML = "";
-    var input = document.getElementById("searchText").value;
+    var input = document.getElementById("inputText").value;
     input = input.toLowerCase();
     if (input === "") {
         ul.innerHTML = "";
+        ul.style.display="none";
         return;
     }
     const searchKeys = getKeysArray(input);
     if (searchKeys.length === 0) {
+        ul.style.display="block";
         const li = makeLiAndOptionalAhrefTag();
         ul.appendChild(li);
         return;
     }
     for (searchKey of searchKeys) {
+        ul.style.display="block";
         var searchResults = searchFor[searchKey];
         for (const result of searchResults) {
             const li = makeLiAndOptionalAhrefTag(result.href, result.innerText);

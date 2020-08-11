5a501024d7fe04c155ec3e2419fb4e8ea0ab4194
Rajan <rajan@addope.com>
2020-08-11 16:23:29 -0600

    hopefully bookmark working

--- dist/components/bookmarkButton.js
+++ dist/components/bookmarkButton.js
@@ -13,9 +13,11 @@ class BookmarkButton extends HTMLElement {
         //always use this.getAttribute() in connectedCallback as the attributes might not have been defined yet in constructor.
         if (typeof (Storage) !== undefined) {
             if (localStorage.getItem(this.getAttribute("bookmarkKey"))) {
+                console.log(localStorage.getItem(this.getAttribute("bookmarkKey"))+ "exists");
                 this.value = "Remove Bookmark";
-                this.innerHTML = `<input type="button" class="btn btn-warning" value="${this.value}" id=${this.getAttribute("buttonId")}`;
+                this.innerHTML = `<input type="button" class="btn btn-warning" value="${this.value}" id=${this.getAttribute("buttonId")}>`;
             } else {
+                console.log(localStorage.getItem(this.getAttribute("bookmarkKey"))+ "dne");
                 this.value = "Add Bookmark";
                 this.innerHTML = `<input type="button" class="btn btn-link" value="${this.value}" id=${this.getAttribute("buttonId")}>`;
             }
